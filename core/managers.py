421b9133034d8e4b920a02b5214d48f382a1f103
Amrittttt <105008027+Amrittttt@users.noreply.github.com>
2026-04-23 19:40:47 +0530

    Update managers.py

--- core/managers.py
+++ core/managers.py
@@ -1,1 +1,18 @@
 
+from django.core.exceptions import ImproperlyConfigured
+from django.db import models
+
+from .tenant_context import get_current_tenant
+
+
+class TenantQuerySet(models.QuerySet):
+    def for_current_tenant(self):
+        tenant = get_current_tenant()
+        if tenant is None:
+            raise ImproperlyConfigured('Tenant context is not set for this request.')
+        return self.filter(tenant=tenant)
+
+
+class TenantManager(models.Manager.from_queryset(TenantQuerySet)):
+    def get_queryset(self):
+        return super().get_queryset().for_current_tenant()
