71aace4d6aa82afcbcd6637bd1c318db0269c38b
Amrittttt <105008027+Amrittttt@users.noreply.github.com>
2026-04-23 19:40:20 +0530

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,10 @@
 
+from django.contrib import admin
+
+from .models import Customer, FailedEmailJob, Order, OrderItem, Tenant
+
+admin.site.register(Tenant)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(FailedEmailJob)
